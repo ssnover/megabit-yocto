29ea0fe10320444d9b368111fcb9a3b366a85cda
Shane Snover <ssnover95@gmail.com>
2025-02-15 01:45:37 -0500

    Initial commit

---
+++ scripts/write-sd.sh
@@ -0,0 +1,1 @@
+sudo bmaptool copy build/tmp/work/raspberrypi0_wifi-poky-linux-gnueabi/core-image-base/1.0/deploy-core-image-base-image-complete/core-image-base-raspberrypi0-wifi.rootfs.wic.bz2 /dev/sda
